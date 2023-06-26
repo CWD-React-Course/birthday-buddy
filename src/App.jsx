91739b35cf98906e1a78dd0b81b971ede01a7298
Curtis W. DeGidio <curtis.degidio@gmail.com>
2023-06-25 19:24:44 -0500

    fixed button width

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <section className="container">
         <h3>{people.length} Birthdays Today</h3>
         <List people={people} />
-        <button className="btn" onClick={clearList}>Clear All</button>
+        <button type="button" className="btn btn-block" onClick={clearList}>Clear All</button>
       </section>
     </main>
   );
